7c0b2b76653671f48f3c80f7cc30d8d1cfc39151
ricardofachinello <ricardomatheu@hotmail.com>
2019-11-13 11:40:41 -0300

    Add files via upload

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild EchoBot.sln -p:DeployOnBuild=true -p:PublishProfile=peloamordedeus-Web-Deploy.pubxml -p:Password=KuQeWmSGlAmEfdQbAF71XenYtHYJWzXFEHzpb8M9MmSzNoJ0AfRco09w2rqP
+
